4ae71c610f2345b49f1cf4abe147d404e5fbdb9d
Lausy <antlau7@gmail.com>
2022-09-25 18:14:53 -0700

    Create champ_tag_filter.py

---
+++ champ_tag_filter.py
@@ -0,0 +1,36 @@
+import random
+import requests
+import pandas as pd
+
+# champion info load
+url = 'https://ddragon.leagueoflegends.com/cdn/12.18.1/data/en_US/champion.json'
+req2 = requests.get(url)
+
+champ_ls = list(req2.json()['data'].keys())
+
+champ_df = pd.DataFrame()
+for i in range(len(champ_ls)):
+    pre_df = pd.json_normalize(req2.json()['data'][champ_ls[i]])
+    champ_df = pd.concat([champ_df, pre_df])
+
+# Create champ pool based on matching tags with role
+role = ['Tank', 'Support']
+champ_pool = []
+# If role is empty, go to else
+if role:
+    for j in range(len(champ_ls)):
+        this_champ = req2.json()['data'][champ_ls[j]]
+        if any(x in this_champ['tags'] for x in role):
+            champ_pool.append(this_champ['name'])
+else:
+    champ_pool = list(champ_df['name'])
+
+print(len(champ_pool))
+print("This is the current champ pool:", champ_pool, "\n")
+
+# Choose champs for team 1 then team 2
+team1 = random.sample(champ_pool, 10)
+team2 = random.sample(list(set(champ_pool) - set(team1)), 10)
+
+print('Champion pool for Team 1:', team1)
+print('Champion pool for Team 2:', team2)
