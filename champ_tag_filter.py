21ecd303397bb370549b157c5b6b7f9e053d1bc1
Lausy <antlau7@gmail.com>
2022-09-27 18:15:17 -0700

    Bot command for tags now available
    Removed on_messages and instead added bot commands which are activated by the prefix !

--- champ_tag_filter.py
+++ champ_tag_filter.py
@@ -3,6 +3,7 @@ import requests
 import pandas as pd
 import discord
 import os
+from discord.ext import commands
 from dotenv import load_dotenv
 
 # Load bot token
@@ -14,70 +15,73 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
+bot = commands.Bot(command_prefix='!', intents=intents)
 # Print to terminal when bot connected
-@client.event
+
+
+@bot.event
 async def on_ready():
     print('bot is locked and loaded')
 
+
+@bot.command()
+async def test(ctx):
+    """Adds two numbers together."""
+    response = 'Fuck you'
+    await ctx.send(response)
+
+
 # Trigger the bot with 'aram+'
-@client.event
-async def on_message(message):
-    # Check if the message is from the bot
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('aram+'):
-        # champion info load
-        url = 'https://ddragon.leagueoflegends.com/cdn/12.18.1/data/en_US/champion.json'
-        req2 = requests.get(url)
+@bot.command(name='aram+', help='Select desired roles i.e. Fighter Support')
 
-        champ_ls = list(req2.json()['data'].keys())
+async def tag_filter(ctx, *tag):
+    tags = ','.join(tag)
 
-        champ_df = pd.DataFrame()
-        for i in range(len(champ_ls)):
-            pre_df = pd.json_normalize(req2.json()['data'][champ_ls[i]])
-            champ_df = pd.concat([champ_df, pre_df])
-        
-        # User input
-        await message.channel.send('What role would you like? (Case sensitive, one role only)')
+    # champion info load
+    url = 'https://ddragon.leagueoflegends.com/cdn/12.18.1/data/en_US/champion.json'
+    req2 = requests.get(url)
 
-        all_roles = ['Tank', 'Support', 'Marksman', 'Fighter', 'Mage']
-        
-        # Check if user input is a valid role
-        def check(m):
-            return m.content in all_roles
+    champ_ls = list(req2.json()['data'].keys())
 
-        # msg is user input from role prompt, 20s timeout
-        msg = await client.wait_for('message', check=check, timeout=20)
-        await message.channel.send(msg.content + 's coming right up')
+    champ_df = pd.DataFrame()
+    for i in range(len(champ_ls)):
+        pre_df = pd.json_normalize(req2.json()['data'][champ_ls[i]])
+        champ_df = pd.concat([champ_df, pre_df])
 
-        # Create champ pool based on matching tags with role
-        role = []
-        role.append(msg.content)
-        champ_pool = []
-        # If role is empty, go to else
-        if role:
-            for j in range(len(champ_ls)):
-                this_champ = req2.json()['data'][champ_ls[j]]
-                if any(x in this_champ['tags'] for x in role):
-                    champ_pool.append(this_champ['name'])
-        else:
-            champ_pool = list(champ_df['name'])
+    all_roles = ['Tank', 'Support', 'Marksman', 'Fighter', 'Mage', '']
 
-        #print("This is the current champ pool:", champ_pool, "\n")
+    # Check if user input is a valid role
+    def check(m):
+        return m.content in all_roles
 
-        # Choose champs for team 1 then team 2
-        pool_size = 10
-        team1 = random.sample(champ_pool, pool_size)
-        team2 = random.sample(list(set(champ_pool) - set(team1)), pool_size)
+    # Create champ pool based on matching tags with role
+    role = tags.split(',')
+    champ_pool = []
+    # If role is empty, go to else
+    if role:
+        for j in range(len(champ_ls)):
+            this_champ = req2.json()['data'][champ_ls[j]]
+            if any(x in this_champ['tags'] for x in role):
+                champ_pool.append(this_champ['name'])
+    else:
+        champ_pool = list(champ_df['name'])
 
-        #print('Champion pool for Team 1:', team1)
-        #print('Champion pool for Team 2:', team2)
 
-        # Tell your friends because we made it
-        await message.channel.send('Champion pool for Team 1')
-        await message.channel.send(team1)
-        await message.channel.send('Champion pool for Team 2')
-        await message.channel.send(team2)
+    print("This is the current champ pool:", champ_pool, "\n")
 
-client.run(TOKEN)
+    # Choose champs for team 1 then team 2
+    pool_size = 10
+    team1 = random.sample(champ_pool, pool_size)
+    team2 = random.sample(list(set(champ_pool) - set(team1)), pool_size)
+
+    rejoice = '''Champion pool for Team 1:\n
+                    {}
+                    \nChampion pool for Team 2:\n
+                    {}'''.format(team1, team2)
+
+    # Tell your friends because we made it
+    await ctx.send(rejoice)
+
+
+bot.run(TOKEN)
